fdb1e530ae20112acd4e3fa2daa8f82ae7e0670e
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-05-25 00:00:19 -0600

    seeds.sql created

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+USE employeeTrackerDB;
+
+INSERT INTO employee (firstName, lastName, roleId, managerId)
+VALUES ();
+
+INSERT INTO department (name)
+VALUES ();
+
+INSERT INTO role (title, salary, departmentId)
+VALUES ();
