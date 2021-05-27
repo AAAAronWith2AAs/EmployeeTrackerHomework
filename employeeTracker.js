df78132503f440918576364e5f5c528a63502d51
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-05-27 00:01:05 -0600

    require console.table

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const { prompt } = require("inquirer");
+const { printTable } = require("console-table-printer");
 
 const connection = mysql.createConnection({
   host: "localhost",
