af0b3134b220eddf33e05ed5f2ba28faaf03b927
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-05-29 19:50:30 -0600

    schema consolidate

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,3 @@
--- * **employee**:
-
---   * **id** - INT PRIMARY KEY
---   * **first_name** - VARCHAR(30) to hold employee first name
---   * **last_name** - VARCHAR(30) to hold employee last name
---   * **role_id** - INT to hold reference to role employee has
---   * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
-
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 USE employees;
@@ -22,22 +14,13 @@ CREATE TABLE employee (
     CONSTRAINT FKmanager FOREIGN KEY(managerId) REFERENCES employee(id) ON DELETE SET NULL
 );
 
--- * **department**:
 
---   * **id** - INT PRIMARY KEY
---   * **name** - VARCHAR(30) to hold department name
 CREATE TABLE department (
     id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 
 );
 
--- * **role**:
-
---   * **id** - INT PRIMARY KEY
---   * **title** -  VARCHAR(30) to hold role title
---   * **salary** -  DECIMAL to hold role salary
---   * **department_id** -  INT to hold reference to department role belongs to
 
 CREATE TABLE role (
     id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
